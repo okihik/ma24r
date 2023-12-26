9f148b024184d63a4988b81a118003f3321e1eb4
okihik <okihik@gmail.com>
2023-12-26 09:10:36 -0800

    1225

---
+++ plotPCAresults.R
@@ -0,0 +1,16 @@
+plotPCAresults <- function(recipe, dat = bean_validation) {
+  library(ggforce)
+  library(dplyr)
+  recipe %>%
+    # Estimate any additional steps
+    prep() %>%
+    # Process the data (the validation set by default)
+    bake(new_data = dat) %>%
+    # Create the scatterplot matrix
+    ggplot(aes(x = .panel_x, y = .panel_y, color = class, fill = class)) +
+    geom_point(alpha = 0.4, size = 0.5) +
+    geom_autodensity(alpha = .3) +
+    facet_matrix(vars(-class), layer.diag = 2) + 
+    scale_color_brewer(palette = "Dark2") + 
+    scale_fill_brewer(palette = "Dark2")
+}
